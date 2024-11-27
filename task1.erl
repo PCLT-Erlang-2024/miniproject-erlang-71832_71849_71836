b6ea7acab8fb931191f5a4d8947ae251229df21f
irene2003 <irenecastrillomar@gmail.com>
2024-11-27 13:13:27 +0100

    final

--- task1.erl
+++ task1.erl
@@ -5,9 +5,13 @@ start() ->
     spawn(task1, factory, []).
 
 factory() ->
-    %% Creat 3 belts and 4 trucks
-    Trucks = [spawn(task1, loop_truck, [Id,0]) || Id <- lists:seq(1, 4)],
-    Belts = [spawn(task1, loop_belt, [Id,Trucks,ok]) || Id <- lists:seq(1, 3)],
+    %% Constants
+    NUM_TRUKS = 5,
+    NUM_BELTS = 3,
+
+    %% Creat 3 belts and 5 trucks
+    Trucks = [spawn(task1, loop_truck, [Id,0]) || Id <- lists:seq(1, NUM_TRUKS)],
+    Belts = [spawn(task1, loop_belt, [Id,Trucks,ok]) || Id <- lists:seq(1, NUM_BELTS)],
     
     loop_factory(Belts, 0).
 
@@ -17,32 +21,34 @@ loop_factory(Belts, Id) ->
 
     %% Sending packages to the belt randomly
     Belt = lists:nth(rand:uniform(length(Belts)), Belts),
-    Belt ! Package,
+    Belt ! {pass, Package},
     io:format("Factory: Package ~p added in the belt~n", [Id+1]),
-    timer:sleep(1000),
+    timer:sleep(50),
     loop_factory(Belts, Id+1).
 
 
 loop_belt(Id, Trucks, Ctrl) ->
     Truck = lists:nth(rand:uniform(length(Trucks)), Trucks),
 
+    timer:sleep(80),
     case Ctrl of
         ok ->
             receive
-                {package, PackId} ->
+                {pass, {package, PackId}} ->
                     io:format("Belt ~p: Package ~p added~n", [Id, PackId]),
                     NewPackage = {package, PackId}
             end;
 
-        {package, PackId}  ->
+        {false, PackId}  ->
             NewPackage = {package, PackId},
-            io:format("Belt ~p: Redistributing package ~p ~n ",[Id, PackId])
+            io:format("Belt ~p: Redistributing package ~p ~n",[Id, PackId])
 
     end,
 
     Truck ! {NewPackage, self()},
+
     receive
-      NewCtrl ->
+      {done, NewCtrl} ->
         loop_belt(Id, Trucks, NewCtrl)
     end.
 
@@ -52,14 +58,14 @@ loop_truck(Id, Capacity) ->
         {{package, PackId}, Belt} ->
 
             if 
-                Capacity > 10 ->   %% MAX CAPACITY TEN
-                    Belt ! {package, PackId},
-                    io:format("Truck ~p is full, dispatching with 10 packages~n", [Id]),
+                Capacity > 6 ->   %% MAX CAPACITY SIX
+                    io:format("Truck ~p is full, dispatching with 6 packages~n", [Id]),
+                    Belt ! {done, {false, PackId}},
                     loop_truck(Id, 0);
            
                 true ->
-                    Belt ! ok,
-                    io:format("Truck ~p: Loaded ~p package~n", [Id, PackId]),
+                    Belt ! {done, ok},
+                    io:format("Truck ~p: Loaded package ~p~n", [Id, PackId]),
                     loop_truck(Id, Capacity+1)
                 end
     end.
